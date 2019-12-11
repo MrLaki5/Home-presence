13fb54883ed929a0ebec755159ef9848d8d07489
MilanLazarevic <milan@darwindigital.com>
2019-12-11 17:00:46 +0100

    added worker with set of addresses

--- app/api/utils/mac_tools.py
+++ app/api/utils/mac_tools.py
@@ -1,10 +1,13 @@
 import re
 import subprocess
+import os
+
+network_mask = os.environ['NETWORK_MASK']
 
 
 def get_active_mac_addresses():
     # Use arp to get all mac addresses on local network
-    addresses = subprocess.check_output(["cat", "/proc/net/arp"])
+    addresses = subprocess.check_output(["nmap", "-sn", network_mask])
     # Use regex to extract all mac addresses
     p = re.compile(r'(?:[0-9a-fA-F]{1,}(?:\-|\:)){5}[0-9a-fA-F]{1,}')
     mac_addresses = re.findall(p, str(addresses))
--- app/api/utils/mac_worker.py
+++ app/api/utils/mac_worker.py
@@ -11,10 +11,29 @@ class MacWorker(threading.Thread):
         self.app_context = app.app_context()
         # Sleep time In seconds
         self.sleep_time = 10
+        self.active_set = []
 
     def run(self):
         while True:
             sleep(self.sleep_time)
             active_mac_addresses = get_active_mac_addresses()
+            for set_mac in self.active_set:
+                if set_mac["mac"] in active_mac_addresses:
+                    set_mac["count"] += 1
+                    active_mac_addresses.remove(set_mac["mac"])
+                else:
+                    set_mac["count"] = 0
+            for active_mac in active_mac_addresses:
+                self.active_set.append({"mac": active_mac, "count": 1})
+            self.active_set = sorted(self.active_set, key=lambda x: x["count"], reverse=True)
             with self.app_context:
-                current_app.logger.debug("Mac addresses: " + str(active_mac_addresses))
+                num = 1
+                current_app.logger.debug("======================================================")
+                current_app.logger.debug("Addresses list:")
+                current_app.logger.debug("======================================================")
+                for item in self.active_set:
+                    current_app.logger.debug("{:0>2d}".format(num) +
+                                             ". Mac: " + str(item["mac"]) +
+                                             ", Count: " + str(item["count"])
+                                             )
+                    num += 1
