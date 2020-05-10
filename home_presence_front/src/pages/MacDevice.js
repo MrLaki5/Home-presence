f0de4a217021d7ec14cb95225c34ee203d78c265
MilanLazarevic <mimar1207@gmail.com>
2020-05-10 13:50:45 +0200

    add: Frontend: MacDevice page user mac info

--- home_presence_front/src/pages/MacDevice.js
+++ home_presence_front/src/pages/MacDevice.js
@@ -2,6 +2,23 @@ import React from 'react';
 import { Redirect } from "react-router-dom";
 import TimeLine from '../components/TimeLine';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+
+import Menu_HP from '../components/Menu_HP'
+import Title_HP from '../components/Title_HP'
+import '../app.css'
+
 
 class MacDevice extends React.Component {
 
@@ -106,37 +123,109 @@ class MacDevice extends React.Component {
         const times = this.state.times.map((time_curr, index) =>
             <TimeLine key={index} time={time_curr}/>
         );
-        return  <div>
-                    <div>
-                        {this.props.location.state.mac}
-                    </div>
-                    <div>
-                        <input type="text" value={this.state.name} onChange={this.handleNameChange} />
-                        <button onClick={this.sendNameChange}> Change name</button>
-                    </div>
-                    <div>
-                        <input
-                            type="range"
-                            min="1"
-                            max="20"
-                            value={this.state.top_val}
-                            onChange={this.handleInputChange}
+        return (
+            <Grid container className='MainContainer'>
+                
+                {/* Title */}
+                <Title_HP/>
+
+
+                {/* Menu */}
+                <Menu_HP current_page={0}/> 
+
+                {/* Back button */}
+                <Grid container item xs={12} style={{marginBottom: '1.5%'}}>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                    <Grid item xs={12} md={4}>
+                        <Hidden only={['xs', 'sm']}>
+                            {/* Button PC */}
+                            <Button size='small' disableRipple={true} fullWidth style={{fontSize: '1vw', fontFamily: 'Collegia', borderRadius: '0%', color: "var(--main-bg-color)", backgroundColor: "var(--main-primary-color)"}} onClick={ () => this.goBack()}>Back</Button>
+                        </Hidden>
+                        <Hidden only={['md', 'lg', 'xl']}>
+                            {/* Button Mobile */}
+                            <Button size='small' disableRipple={true} fullWidth style={{fontSize: '3vw', fontFamily: 'Collegia', borderRadius: '0%', color: "var(--main-bg-color)", backgroundColor: "var(--main-primary-color)"}} onClick={ () => this.goBack()}>Back</Button>
+                        </Hidden>
+                    </Grid>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                </Grid>
+
+                {/* User and mac info */}
+                <Grid container item xs={12}>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                    <Grid item xs={12} md={4}>
+                        <TextField
+                            id="standard-full-width"
+                            disabled={true}
+                            label="MAC"
+                            style={{color: "var(--main-primary-color)" }}
+                            placeholder="MAC"
+                            fullWidth
+                            margin="normal"
+                            defaultValue={this.props.location.state.mac}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
                         />
-                        <select
-                            value={this.state.time_group}
-                            onChange={this.handleSelectTimeChange}
-                        >
-                            <option value="hour">Hour</option>
-                            <option value="day">Day</option>
-                            <option value="month">Month</option>
-                            <option value="year">Year</option>
-                        </select>
-                    </div>
-                    <div>
-                        {times}
-                    </div>
-                    <a onClick={this.goBack}> Back </a>
+                    </Grid>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                    <Grid item xs={12} md={4}>
+                        <TextField
+                            id="standard-full-width"
+                            label="User"
+                            style={{color: "var(--main-primary-color)" }}
+                            placeholder="User"
+                            fullWidth
+                            margin="normal"
+                            onChange={this.handleNameChange}
+                            defaultValue={this.state.name}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </Grid>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                    
+                    {/* Change button */}
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+                    <Grid item xs={12} md={4}>
+                        <Hidden only={['xs', 'sm']}>
+                            {/* Button PC */}
+                            <Button size='small' disableRipple={true} fullWidth style={{fontSize: '1vw', fontFamily: 'Collegia', borderRadius: '0%', color: "var(--main-bg-color)", backgroundColor: "var(--main-primary-color)"}} onClick={ () => this.sendNameChange()}>Save</Button>
+                        </Hidden>
+                        <Hidden only={['md', 'lg', 'xl']}>
+                            {/* Button Mobile */}
+                            <Button size='small' disableRipple={true} fullWidth style={{fontSize: '3vw', fontFamily: 'Collegia', borderRadius: '0%', color: "var(--main-bg-color)", backgroundColor: "var(--main-primary-color)"}} onClick={ () => this.sendNameChange()}>Save</Button>
+                        </Hidden>
+                    </Grid>
+                    <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+
+                </Grid>
+
+
+                <div>
+                    <input
+                        type="range"
+                        min="1"
+                        max="20"
+                        value={this.state.top_val}
+                        onChange={this.handleInputChange}
+                    />
+                    <select
+                        value={this.state.time_group}
+                        onChange={this.handleSelectTimeChange}
+                    >
+                        <option value="hour">Hour</option>
+                        <option value="day">Day</option>
+                        <option value="month">Month</option>
+                        <option value="year">Year</option>
+                    </select>
                 </div>
+                <div>
+                    {times}
+                </div>
+
+            </Grid>)
     }
 }
 
