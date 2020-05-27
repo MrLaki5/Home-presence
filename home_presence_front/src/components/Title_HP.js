5d6d37fa44614f53317cc31006f17e1c568a272c
Milan Lazarevic <mimar1207@gmail.com>
2020-05-27 20:27:10 +0200

    add:frontend: version

--- home_presence_front/src/components/Title_HP.js
+++ home_presence_front/src/components/Title_HP.js
@@ -8,19 +8,40 @@ import Hidden from '@material-ui/core/Hidden';
 
 function Title_HP(props) {
     return (
-        <Grid item xs={12}>
-            <Hidden only={['xs', 'sm']}>
-                {/* Title PC */}
-                <div className='Title'>
-                    Home-Presence
-                </div>
-            </Hidden>
-            <Hidden only={['md', 'lg', 'xl']}>
-                {/* Title Mobile */}
-                <div className='TitleMobile'>
-                    Home-Presence
-                </div>
-            </Hidden>
+        <Grid item container xs={12}>
+            <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+            <Grid item xs={12} md={4}>
+                <Hidden only={['xs', 'sm']}>
+                    {/* Title PC */}
+                    <div className='Title'>
+                        Home-Presence
+                    </div>
+                </Hidden>
+                <Hidden only={['md', 'lg', 'xl']}>
+                    {/* Title Mobile */}
+                    <div className='TitleMobile'>
+                        Home-Presence
+                    </div>
+                </Hidden>
+            </Grid>
+            <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+
+            <Grid item xs={12} md={4} className="VersionContainer">
+                <Hidden only={['xs', 'sm']}>
+                    {/* Title PC */}
+                    <div className='TitleVersion'>
+                        MrLaki5 v{process.env.REACT_APP_VERSION}
+                    </div>
+                </Hidden>
+                <Hidden only={['md', 'lg', 'xl']}>
+                    {/* Title Mobile */}
+                    <div className='TitleVersionMobile'>
+                        MrLaki5 v{process.env.REACT_APP_VERSION}
+                    </div>
+                </Hidden>
+            </Grid>
+            <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
+            <Grid item only={['md', 'lg', 'xl']} md={4}></Grid>
         </Grid>
     );
 }
